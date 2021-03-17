baa2686402aec9f88598460c1e6eb835bd872b64
enigmaticrex <sain.sajix@gmail.com>
2021-03-17 14:16:04 +0530

    Testing Container Set

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Container(
+          color: Colors.white,
+          child: Text("Hello Flutter"),
+        ),
       ),
     ),
   );
