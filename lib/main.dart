065bde2c7bee784c03b0851ae13ff18861b98397
enigmaticrex <sain.sajix@gmail.com>
2021-03-17 21:40:05 +0530

    Update main.dart
    Setting container

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,35 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("My Bussiness Card")),
+          backgroundColor: Colors.orangeAccent,
+        ),
         backgroundColor: Colors.teal,
-        body: Container(
-          color: Colors.white,
-          child: Text("Hello Flutter"),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.white,
+                margin: EdgeInsets.all(20.0),
+                child: Center(
+                  child: Text("Hello Dart"),
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                child: Center(
+                  child: Text("Hello Flutter"),
+                ),
+                width: 100.0,
+                height: 100.0,
+                margin: EdgeInsets.all(20.0),
+                // padding: EdgeInsets.all(25.0),
+              )
+            ],
+          ),
         ),
       ),
     ),
