e61b491f041b9a2a311580f4aa182603f25c3f7d
enigmaticrex <sain.sajix@gmail.com>
2021-03-25 11:18:13 +0530

    Finished Card

--- lib/main.dart
+++ lib/main.dart
@@ -11,30 +11,86 @@ void main() {
         ),
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                width: 100,
-                height: 100,
+            child: Column(
+          children: [
+            SizedBox(
+              height: 10.0,
+            ),
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.white,
+              backgroundImage: NetworkImage(
+                  'https://svn.apache.org/repos/infra/websites/production/ooo-site/content/images/aoo-logo-100x100.png'),
+            ),
+            Text(
+              'Sain Saji',
+              style: TextStyle(
                 color: Colors.white,
-                margin: EdgeInsets.all(20.0),
-                child: Center(
-                  child: Text("Hello Dart"),
+                fontSize: 20.0,
+              ),
+            ),
+            Text(
+              'DESIGNER',
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone_android,
+                      size: 20.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+91 9656476863',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Container(
-                color: Colors.white,
-                child: Center(
-                  child: Text("Hello Flutter"),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email_rounded,
+                      size: 20.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'sain.sajix@gmail.com',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
                 ),
-                width: 100.0,
-                height: 100.0,
-                margin: EdgeInsets.all(20.0),
-                // padding: EdgeInsets.all(25.0),
-              )
-            ],
-          ),
-        ),
+              ),
+              color: Colors.white,
+            )
+          ],
+        )),
       ),
     ),
   );
